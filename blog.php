deb2290dc46a96a8f2d4db59a90eeb1511b0978f
George O <16269580+Ge0rg3@users.noreply.github.com>
2020-11-04 03:08:13 +0000

    Add blog page styling

--- blog.php
+++ blog.php
@@ -4,45 +4,34 @@
         <h5>Breaking the Competition (Bug Bounty Write-up)</h5>
         <p><i>Wednesday, 15th January 2020</i></p>
         <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-        <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-        <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-        <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-        <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
+        <p><i><a href="https://medium.com/ctf-writeups/breaking-the-competition-bug-bounty-write-up-ca7cb7bc53f5">Read more...</a></i></p>
 </div>
-
 <hr>
 
 <div class="row">
-    <h5>Breaking the Competition (Bug Bounty Write-up)</h5>
-    <p><i>Wednesday, 15th January 2020</i></p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
+        <h5>BLK_BOX Challenge Write-up</h5>
+        <p><i>Wednesday, 15th January 2020</i></p>
+        <p>Around 6 months ago, HMGCC released a challenge and offered a challenge coin to anyone who completed it. I started pretty late on, but managed to get it done in time. This consisted of 7 stages, ranging from reverse engineering to traffic analysis. At the time of writing, you can still have a go yourself here.</p>
+        <p><i><a href="https://medium.com/ctf-writeups/blk-box-challenge-write-up-abc9a091142c">Read more...</a></i></p>
 </div>
-
 <hr>
 
 <div class="row">
-    <h5>Breaking the Competition (Bug Bounty Write-up)</h5>
-    <p><i>Wednesday, 15th January 2020</i></p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
+        <h5>Sunshine CTF 2019 Write-up</h5>
+        <p><i>Wednesday, 15th January 2020</i></p>
+        <p>At the end of March this year, Hack@UCF released a CTF in collaboration with BSides Orlando 2019. Our team ended up coming 13th, narrowly missing out on a top 10 spot. You can find the homepage for this CTF here.</p>
+        <p><i><a href="https://medium.com/ctf-writeups/sunshine-ctf-2019-write-up-c7174c0fb56">Read more...</a></i></p>
 </div>
-
 <hr>
 
 <div class="row">
-    <h5>Breaking the Competition (Bug Bounty Write-up)</h5>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
-    <p>In this post, I'll be describing how I found 5 bugs on a private HackerOne program. The website that I attacked was a new CTF hosting provider, and I had actually participated in a CTF using this provider prior to being invited to their private program.</p>
+        <h5>StegOnline: A New Steganography Tool</h5>
+        <p><i>Wednesday, 15th January 2020</i></p>
+        <p>Over the last couple of months, I have been developing an online image Steganography tool designed to combine and enhance the features of other separate tools. It's open-source and due to the nature of Angular, it's easy to add to.</p>
+        <p><i><a href="https://medium.com/ctf-writeups/stegonline-a-new-steganography-tool-b4eddb8f8f57">Read more...</a></i></p>
 </div>
+<hr>
+
+<p><a href="https://medium.com/@georgeomnet">Read the rest over on medium.</a></p>
 
 <?php require("includes/footer.php") ?>
