3c904ea83821a7b73fee0bf84f9cc432d5b5bdd3
George O <16269580+Ge0rg3@users.noreply.github.com>
2020-11-06 01:34:51 +0000

    Add bug bounty page

---
+++ public_html/bounty.php
@@ -0,0 +1,40 @@
+<?php require("../includes/header.php") ?>
+
+<h3>Bug Bounty Policy</h3>
+
+<h5>Description</h5>
+<p class="bottom-0">As someone who regularly participates in bug bounties, it's only fair that my assets have a policy themselves.</p>
+<p>Please feel free to report any bug found under the <code>*.georgeom.net</code> domain (unless stated below) or on my <a href="https://github.com/Ge0rg3/">GitHub profile</a> to <a href="mailto:georgeomnet@gmail.com">georgeomnet@gmail.com</a>.</p>
+
+<h5>Assets</h5>
+<p>All <code>*.georgeom.net</code> domains are valid (unless mentioned further down), but here's a list of my current assets:</p>
+<ul>
+    <li>georgeom.net</li>
+    <li>www.georgeom.net</li>
+    <li>admin.georgeom.net</li>
+    <li>direct.georgeom.net</li>
+    <li>stegonline.georgeom.net</li>
+    <li>mobsf.georgeom.net</li>
+    <li>old.georgeom.net</li>
+    <li>cipher.georgeom.net (<strong>webapp only</strong>, otherwise report to <a href="https://docs.repl.it/misc/security">repl.it</a>)</li>
+    <li>gci.georgeom.net (<strong>webapp only</strong>, otherwise report to <a href="https://docs.repl.it/misc/security">repl.it</a>)</li>
+    <li>cloud.georgeom.net</li>
+    <li>kf2.georgeom.net</li>
+</ul>
+
+<h5>Out of Scope</h5>
+<p>The following bugs are <strong>out of scope</strong>:</p>
+<ul>
+    <li>Real server IP discovery. The site is behind cloudflare for DNS, caching and WAF, not for IP masking. Hosts can only be accessed from cloudflare IPs anyway, and <code>direct.georgeom.net</code> simply points to the true server IP.</li>
+    <li>Missing best-practice headers on sites (other than <code>georgeom.net</code> and <code>www.georgeom.net)</code>.</li>
+    <li>Non-critical issues on <code>cloud.georgeom.net</code> - issues should be reported to <a href="https://nextcloud.com">Nextcloud</a>.</li>
+    <li>Any email-related issues on an <code>@georgeom.net</code> email address.</li>
+</ul>
+
+<h5>Rewards</h5>
+<p>If anything is found that's P3 or above (according to the <a href="https://bugcrowd.com/vulnerability-rating-taxonomy">Bugcrowd VRT</a>), I'll send some stickers your way and list your name on this page.</p>
+
+<br>
+
+
+<?php require("../includes/footer.php") ?>
--- public_html/competitions.php
+++ public_html/competitions.php
@@ -34,25 +34,26 @@
 </div>
 
 <h5>Solo</h5>
-<em>N/A</em>
+<strong>2020</strong>
 <ul>
+    <li><a href="https://medium.com/cyber-discovery/cyber-discovery-content-creators-club-6a0f79fe7144">CyberDiscovery Content Creators Club CTF</a>, 7th Place</li>
 </ul>
 
 <br>
 
 <h5>Team</h5>
-<strong><a href="https://ctftime.org/team/78898#rating_2020">2020</a></strong>
+<strong>2020</strong>
 <ul>
-    <li>SANS SEC503 NetWars</a>, 4th Place</li>
+    <li><a href="https://www.sans.org/netwars/dfir-tournament">SANS DFIR NetWars</a>, 4th Place</li>
     <li><a href="https://ctftime.org/event/1023">X-MAS GTF</a>, 2nd Place</li>
     <li><a href="https://ctftime.org/event/928">TJCTF</a>, 13th Place</li>
     <li><a href="https://ctftime.org/event/1051">RACTF</a>, 18th Place</li>
     <li><a href="https://ctftime.org/event/997">Houseplant CTF</a>, 56th Place</li>
 </ul>
 
-<strong><a href="https://ctftime.org/team/78898#rating_2019">2019</a></strong>
+<strong>2019</strong>
 <ul>
-    <li>Cyberstart Cloud CTF, 1st Place</li>
+    <li><a href="https://joincyberdiscovery.com/">Cyberstart Cloud CTF</a>, 1st Place</li>
     <li><a href="https://ctftime.org/event/774">ångstromCTF</a>, 40th Place</li>
     <li><a href="https://ctftime.org/event/834">redpwnCTF</a>, 124th Place</li>
 
