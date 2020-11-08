0cea92a48aa969857310ddc0e79b374fa74dd116
George Omnet <george.omnet@accenture.com>
2020-11-08 19:27:50 +0000

    Google site verification

--- includes/header.php
+++ includes/header.php
@@ -20,6 +20,7 @@
         <link rel="stylesheet" href="assets/third-parties/skeleton.css">
         <link rel="stylesheet" href="assets/third-parties/dark-skeleton.css">
         <link rel="stylesheet" href="assets/style.css">
+        <meta name="google-site-verification" content="7GT6boD82vaGq30OJUVrLAufphRmk5bGQjGkPzg8H6c" />
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-QLW6MQ2S6Z"></script>
         <script>
