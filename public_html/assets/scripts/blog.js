ac2fe01e027c1cf9be813d83cd199de667b39697
George <16269580+Ge0rg3@users.noreply.github.com>
2021-11-16 14:49:39 +0000

    Blog sorting and filtering

--- public_html/assets/scripts/blog.js
+++ public_html/assets/scripts/blog.js
@@ -1,18 +1,85 @@
+// Get blog posts from JSON file
+let posts = [];
 fetch("/data/blog.json").then((res) => {
-    res.json().then((posts) => {
-        console.info(`${posts.length} posts loaded.`);
-        let postsList = document.getElementById("blog-posts");
-        for (let post of posts) {
-            const date = new Date(post.date*1000).toUTCString();
-            postsList.innerHTML += `
-            <div class="row">
-            <h5>${post.title}</h5>
-            <p><i>${date}</i></p>
-            <p>${post.preview}</p>
-            <p><i><a href="${post.link}">Read more...</a></i></p>
-            </div>
-            <hr style="width: 100%">
-            `;
-        }
+    res.json().then((retrievedPosts) => {
+        console.info(`${retrievedPosts.length} posts loaded.`);
+        posts = retrievedPosts;
+        updatePosts();
     })
 })
+
+function updatePosts() {
+    // Get filters from query ps
+    const currentParams = new URLSearchParams(location.search);
+    const category = currentParams.get("category");
+    const year = currentParams.get("year");
+    const source = currentParams.get("source");
+    // Filter blog posts
+    filteredPosts = posts.filter((post) => {
+        // Check year matches
+        const postYear = (new Date(post.date*1000)).getFullYear();
+        if (year !== null && year !== postYear + "") return false;
+        // Check source matches
+        if (source !== null && source !== post.source) return false;
+        // Check category matches
+        if (category !== null && category !== post.category) return false;
+        return true;
+    })
+    // Add to element
+    let postsList = document.getElementById("blog-posts");
+    postsList.innerHTML = "";
+    for (let post of filteredPosts) {
+        const date = new Date(post.date*1000).toUTCString();
+        postsList.innerHTML += `
+        <div class="row">
+        <h5>${post.title}</h5>
+        <p><i>${date}</i></p>
+        <p>${post.preview}</p>
+        <p><i><a href="${post.link}">Read more...</a></i></p>
+        </div>
+        <hr style="width: 100%">
+        `;
+    }
+}
+
+// Add change listeners on selects
+let selects = Array.from(document.getElementsByTagName("select"));
+selects.forEach((select) => {
+    select.addEventListener("change", function (evt) {
+        // Get the URL parameter based off select type
+        let queryParameter;
+        switch (evt.currentTarget.id) {
+            case "category-select":
+                queryParameter = "category";
+                break;
+            case "year-select":
+                queryParameter = "year";
+                break;
+            case "source-select":
+                queryParameter = "source";
+                break;
+            default:
+                return;
+        }
+        // Find current url and param stats
+        let baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        let currentParams = new URLSearchParams(location.search);
+        // Update query params
+        if (evt.target.value == "") {
+            currentParams.delete(queryParameter);
+        } else {
+            currentParams.set(queryParameter, evt.currentTarget.value);
+        }
+        // Add new selection criteria to URL
+        const newUrl = baseUrl + (currentParams.toString().length > 0 ? "?" : "") + currentParams.toString();
+        window.history.pushState({path: newUrl}, "", newUrl);
+        // Reload blog items
+        updatePosts();
+    })
+})
+
+// Set default selects from query ps
+const queryParams = new URLSearchParams(location.search);
+document.getElementById("category-select").value = queryParams.get("category") || "";
+document.getElementById("year-select").value = queryParams.get("year") || "";
+document.getElementById("source-select").value = queryParams.get("source") || "";
--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -15,40 +15,57 @@ if len(argv) < 2:
 else:
     output_filepath = argv[1]
 
-# Parse RSS Feed
-feed = parse(RSS_URL)
-posts = feed.entries
+def getMedium():
+    # Parse RSS Feed
+    feed = parse(RSS_URL)
+    posts = feed.entries
 
-# Convert to dict
-parsed_posts = []
-for post in posts:
-    # If no tags in post, not meant to be shared
-    if len(post.get("tags", [])) == 0:
-        continue
-    # Get summary
-    preview = post.get("summary")
-    # Replace nbsp with space
-    preview = preview.replace("\xa0", " ")
-    # Remove italic and header tags
-    soup = BeautifulSoup(preview, "html.parser")
-    for tag in soup.find_all(["em", "h3"]):
-        tag.replaceWith("")
-    # Extract text from HTML
-    text = " ".join(soup.findAll(text=True))
-    # Fix spacing issues in sentences (sometimes space before ".")
-    text = text.replace(" .", "")
-    # Get only first 4 sentences
-    sentences = [sentence + "." for sentence in text.split(". ")]
-    extracted_text = " ".join(sentences[:4])
-    # Append to total array
-    parsed_posts.append({
-        "title": post.get("title"),
-        "link": post.get("link"),
-        "date": int(mktime(post.get("published_parsed"))),
-        "preview": extracted_text
-    })
+    # Convert to dict
+    parsed_posts = []
+    for post in posts:
+        # If no tags in post, not meant to be shared
+        if len(post.get("tags", [])) == 0:
+            continue
+        # Get summary
+        preview = post.get("summary")
+        # Replace nbsp with space
+        preview = preview.replace("\xa0", " ")
+        # Remove italic and header tags
+        soup = BeautifulSoup(preview, "html.parser")
+        for tag in soup.find_all(["em", "h3"]):
+            tag.replaceWith("")
+        # Extract text from HTML
+        text = " ".join(soup.findAll(text=True))
+        # Fix spacing issues in sentences (sometimes space before ".")
+        text = text.replace(" .", "")
+        # Get only first 4 sentences
+        sentences = [sentence + "." for sentence in text.split(". ")]
+        extracted_text = " ".join(sentences[:4])
+        # "Guesstimate" blog category (old and unreliable, but future blogs are all Markdown)
+        title = post.get("title").upper()
+        if "HTB" in title or "HACK THE BOX" in title:
+            category = "HTB"
+        elif "BOUNTY" in title:
+            category = "BOUNTY"
+        elif "TOOL" in title:
+            category = "TOOL"
+        elif "CTF" in title or "CHALLENGE" in title:
+            category = "CTF"
+        else:
+            category = "OTHER"
+        # Append to total array
+        parsed_posts.append({
+            "title": post.get("title"),
+            "link": post.get("link"),
+            "date": int(mktime(post.get("published_parsed"))),
+            "preview": extracted_text,
+            "source": "Medium",
+            "category": category
+        })
+    return parsed_posts
 
+posts = getMedium()
 # Write to file
 f =  open(output_filepath, "w")
-dump(parsed_posts, f)
+dump(posts, f)
 f.close()
